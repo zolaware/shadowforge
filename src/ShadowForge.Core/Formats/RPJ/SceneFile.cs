51fe00eee8dc7814ee9d9d90107fa838b8b10526
Tom <1568512+tomcl7@users.noreply.github.com>
2026-03-26 23:25:48 -0400

    add typed waypoint properties

--- src/ShadowForge.Core/Formats/RPJ/SceneFile.cs
+++ src/ShadowForge.Core/Formats/RPJ/SceneFile.cs
@@ -57,6 +57,29 @@ public class Waypoint
 {
     public byte[] RawData { get; set; } = new byte[0x40];
     public int FileOffset { get; set; }
+
+    public uint Id => BigEndian.ReadUInt32(RawData, 0x00);
+    public uint WaypointType => BigEndian.ReadUInt32(RawData, 0x04);
+    public uint ConditionFlags => BigEndian.ReadUInt32(RawData, 0x08);
+    public uint ConditionValue => BigEndian.ReadUInt32(RawData, 0x0C);
+    public float PosX => BigEndian.ReadFloat(RawData, 0x10);
+    public float PosY => BigEndian.ReadFloat(RawData, 0x14);
+    public float PosZ => BigEndian.ReadFloat(RawData, 0x18);
+    public float Rotation => BigEndian.ReadFloat(RawData, 0x1C);
+    public byte[] RefData => RawData[0x20..0x30];
+    public uint Target0 => BigEndian.ReadUInt32(RawData, 0x30);
+    public uint Target1 => BigEndian.ReadUInt32(RawData, 0x34);
+    public uint Target2 => BigEndian.ReadUInt32(RawData, 0x38);
+    public uint NextOffset => BigEndian.ReadUInt32(RawData, 0x3C);
+
+    public uint ConditionType => ConditionFlags & 0x0F;
+    public uint ConditionOp => (ConditionFlags >> 4) & 0x0F;
+
+    public bool IsResource => WaypointType == 5;
+    public bool IsSpawn => WaypointType == 6;
+
+    public void WriteBE(int offset, uint value) => BigEndian.WriteUInt32(RawData, offset, value);
+    public void WriteBEFloat(int offset, float value) => BigEndian.WriteFloat(RawData, offset, value);
 }
 
 public class RawGap
